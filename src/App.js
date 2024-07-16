452352e8d931692ae9284c376457d817c5f91025
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-07-16 19:37:49 +0530

    path error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import ChatPage from "./components/ChatPage";
+import ChatPage from "./components/Chatpage";
 
 import Home from "./components/home";
 import { Routes, Route } from "react-router-dom";
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full h-screen flex bg bg-gradient-to-r from-indigo-500 text-center justify-center ">
       <Routes>
         <Route path="/" element={<Home />} exact />
-        <Route path="/ChatPage" element={<ChatPage />} />
+        <Route path="/Chatpage" element={<ChatPage />} />
       </Routes>
     </div>
   );
